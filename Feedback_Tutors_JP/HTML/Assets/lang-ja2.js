d1a3da628c101ab729e699068c63902c3e1ae6aa
Shintaro Sato <123647593+sushitarou19930331@users.noreply.github.com>
2025-08-01 14:16:51 +0900

    Add files via upload

---
+++ Feedback_Tutors_JP/HTML/Assets/lang-ja2.js
@@ -0,0 +1,22 @@
+var CTATLanguagePack =
+{
+        "HORIZONTAL" : "LR", // Accepted values are: LR, RL
+        "VERTICAL" : "TB",      // Accepted values are TB, BT
+        "LOADING" : "Please wait while the tutor is being loaded",
+        "NEXTPROBLEM" : "Retrieving the Next Problem...",
+        "CONGRATULATIONS_YOU_ARE_DONE": "おめでとう！クリアしました。",
+        "SURE_YOU_ARE_DONE": "Are you sure you are done?",
+        "TUTORDISCONNECTED" : "The tutor has disconnected. Please refresh the page.",
+        "OUTOFORDER" : "You need to do other steps first, before doing the step you just worked on. You might request a hint for more help.",
+        "DONE" : "おわり",
+        "HINT" : "ヒント",
+        "NEXT" : "次へ",
+        "PREVIOUS" : "前へ",
+        "HIGHLIGHTEDSTEP" : "Instead of the step you are working on, please work on the highlighted step.",
+        "NOTDONE" : "I'm sorry, but you are not done yet. Please continue working.",
+        "AUTHORPLEASECLOSE" : "Authoring Tools disconnected. Please close this page.",
+        "ERROR_502" : "Error contacting the server, please refresh the page and try again (HTTP status 502: gateway response).",
+        "ERROR_CONN_TS" : "Connection refused by tutoring service, please check your server or contact an administrator.",
+        "ERROR_CONN_LS" : "Connection refused by the logging server, please check your connection or contact an administrator.",
+        "AWAITING_OTHER_COLLABORATORS": "Waiting for other team members..."
+};
